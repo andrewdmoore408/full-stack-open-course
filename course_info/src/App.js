c985cc7243f7fa7a945b3cd9955a8d919dc15bc1
Andrew Moore <andrewdmoore84@gmail.com>
2022-03-21 00:00:43 -0400

    Refactor App components so that Header, Content (using Part), and Total are separate components

--- course_info/src/App.js
+++ course_info/src/App.js
@@ -1,25 +1,54 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  );
+};
+
+const Content = (props) => {
+  return (
+    props.parts.map(part => <Part part={part} />)
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.numExercises}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises: {props.totalExercises}
+    </p>
+  );
+};
+
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      numExercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      numExercises: 7,
+    },
+    {
+      name: 'State of a component',
+      numExercises: 14,
+    }
+  ];
+  const totalExercises = parts.reduce((total, part) => total += part.numExercises, 0);
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total totalExercises={totalExercises} />
     </div>
   );
 }
