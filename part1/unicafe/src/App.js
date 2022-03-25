44630fedbcefa83093cedc4acbd9f3bfae4090a2
Andrew Moore <andrewdmoore84@gmail.com>
2022-03-25 01:27:18 -0400

    Add Button and StatLine function components.
    Button takes a function reference to invoke onClick and its text. StatLine takes text and a number (from the state) to output in a paragraph element

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,15 +2,33 @@ import { useState } from 'react';
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [ good, setGood ] = useState(0);
+  const [ neutral, setNeutral ] = useState(0);
+  const [ bad, setBad ] = useState(0);
+
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
 
   return (
     <div>
       <SectionHeader text='give feedback' />
+      <Button onclick={handleGoodClick} text='Good' />
+      <Button onclick={handleNeutralClick} text='Neutral' />
+      <Button onclick={handleBadClick} text='Bad' />
       <SectionHeader text='statistics' />
+      <StatLine text='good' data={good} />
+      <StatLine text='neutral' data={neutral} />
+      <StatLine text='bad' data={bad} />
     </div>
+  );
+};
+
+const Button = ({ onclick, text }) => {
+  return (
+    <button onClick={onclick}>
+      {text}
+    </button>
   );
 };
 
@@ -19,6 +37,14 @@ const SectionHeader = ({ text }) => {
     <h1>
       {text}
     </h1>
+  );
+};
+
+const StatLine = ({ text, data }) => {
+  return (
+    <p>
+      {text}  {data}
+    </p>
   );
 };
 
