42c4747345e7d0970d1402af90745cad56de1fce
Andrew Moore <andrewdmoore84@gmail.com>
2022-04-03 20:18:11 -0400

    Add Contact component; add ability to add a new contact using form

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,41 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Contact from './components/contact';
 
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ]);
-  const [ newName, setNewName ] = useState('');
+  const [ newName, setNewName ] = useState('name to add');
+
+  const handleAddContact = (event) => {
+    event.preventDefault();
+
+    setPersons(persons.concat({name: newName}));
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleAddContact}>
         <div>
-          name: <input />
+          name:
+          <input
+            value={newName}
+            onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map(person => <Contact key={person.name} name={person.name} />)}
+      </div>
     </div>
   );
 }
