cd01d60b4bdd6e9908f613085e7873281121f474
Andrew Moore <andrewdmoore84@gmail.com>
2022-04-18 19:59:17 -0400

    Update App to use example API/DB to add a new note

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -29,11 +29,15 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
+      // id: notes.length + 1,
     };
 
-    setNotes(notes.concat(newNoteObject));
-    setNewNote('');
+    axios
+      .post('http://localhost:3001/notes', newNoteObject)
+      .then(response => {
+        setNotes(notes.concat(newNoteObject));
+        setNewNote('');
+      });
   };
 
   const handleNoteChange = (event) => {
