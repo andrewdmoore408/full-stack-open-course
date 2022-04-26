93e9e159dac08035e812a66bca48cbe396d8e374
Andrew Moore <andrewdmoore84@gmail.com>
2022-04-25 20:04:20 -0400

    Code cleanup: remove unnecessary code and update URL for noteService

--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,21 +1,11 @@
 import axios from 'axios';
 
-const baseUrl = 'https://nameless-hamlet-49700.herokuapp.com/api/notes';
-
-// const getAll = () => {
-//   const request = axios.get(baseUrl);
-//   return request.then(response => response.data);
-// };
+const baseUrl = '/api/notes';
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-  const nonExisting = {
-    id: 10000,
-    content: 'This note is not saved to server',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  }
-  return request.then(response => response.data.concat(nonExisting))
+
+  return request.then(response => response.data);
 }
 
 const create = (newObject) => {
